4625dca09b1a718f389df5d39ca37df069fdccd3
Denis Khabarov <admin@saymon21-root.pro>
2015-08-14 15:05:08 +0300

    sample deploy

---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+set :application, "openapi.kiiiosk.ru"
+set :scm, :git
+set :repo_url, 'git@github.com:BrandyMint/kiiiosk-open-api.git'
+set :keep_releases, 5
+set :branch, ENV['BRANCH'] || 'master'
+namespace :deploy do
+  task  :leinclean do
+    on roles :all do
+      within current_path do
+        execute "cd #{current_path} && lein do clean"
+      end
+    end
+  end
+
+  task  :leindeps do
+    on roles :all do
+      within current_path do
+        execute "cd #{current_path} && lein deps"
+      end
+    end
+  end
+
+  task  :leinuberwar do
+    on roles :all do
+      within current_path do
+        execute "cd #{current_path} && lein ring uberwar"
+      end
+    end
+  end
+
+
+  task  :makewebapps do
+    on roles :all do
+      within current_path do
+        execute "cd #{deploy_to}/current && mkdir -v webapps && cp -v target/server.jar webapps/ROOT.war"
+      end
+    end
+  end
+
+  after :published, 'leinclean'
+  after :leinclean, 'leindeps'
+  after :leindeps, 'leinuberwar'
+  after :leinuberwar, 'makewebapps'
+
+end
