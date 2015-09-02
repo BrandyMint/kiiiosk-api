634f5e4fd33def28031c7851277be2885674f1fb
Sergey Laptev <iamsergeylaptev@gmail.com>
2015-09-02 09:12:36 +0000

    Generate YML refactor

--- src/generate_yml/core.clj
+++ src/generate_yml/core.clj
@@ -1,11 +1,11 @@
 (ns generate-yml.core
   "Functions for generation Yandex-compatible YML files"
   (:require [clojure.java.io :as io]
-            [generate-yml.tags :refer [generate-tags]]))
+            [generate-yml.tree :as tree]))
 
 ; (generate-yml 355 "/tmp/bar.xml")
 (defn generate-yml
   [vendor-id output-path]
-  (let [tags (generate-tags vendor-id)]
+  (let [yml-tree (tree/generate-tree vendor-id)]
     (with-open [out-file (io/writer output-path :encoding "UTF-8")]
-      (.write out-file tags))))
+      (.write out-file yml-tree))))
