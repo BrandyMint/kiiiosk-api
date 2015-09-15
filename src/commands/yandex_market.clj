28cf097043b4079859b6015d681863a8e59c79d9
Sergey Laptev <iamsergeylaptev@gmail.com>
2015-09-15 21:51:30 +0000

    Generate catalogs via rabbitMQ

--- src/commands/yandex_market.clj
+++ src/commands/yandex_market.clj
@@ -2,13 +2,18 @@
   (:require [langohr.core :as lc]
             [langohr.channel :as lch]
             [langohr.basic :as lb]
+            [clojure.tools.logging :as log]
             [config :refer [default-exchange-name ymarket-qname]]))
 
-(defn -main
+(defn notify-command-sent
+  [vendor-id]
+  (-> (format "[x] Sent command for generating Yandex.Market catalog with vendor ID %s"
+              vendor-id)
+      log/info))
+
+(defn start-generate
   [vendor-id]
   (with-open [conn (lc/connect)]
     (let [ch (lch/open conn)]
       (lb/publish ch default-exchange-name ymarket-qname vendor-id)
-      (-> (format "[x] Sent command for generating Yandex.Market catalog with vendor ID %s"
-                  vendor-id)
-          println))))
+      (notify-command-sent vendor-id))))
